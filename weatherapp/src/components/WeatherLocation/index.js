eb3687e5a188c5c1cd3b237d68d51acfaf1a5e91
natalypazm <pazmoralesnataly@gmail.com>
2018-06-21 17:21:12 -0500

    add new changes

--- weatherapp/src/components/WeatherLocation/index.js
+++ weatherapp/src/components/WeatherLocation/index.js
@@ -1,47 +1,71 @@
 import React, { Component } from 'react';
+import convert from 'convert-units';
 import Location from './Location';
 import WeatherData from './WeatherData';
 import{SUN, CLOUDY, WINDY} from './../../constants/weather';
 import './style.css';
 
+const location = "Arequipa,pe";
+const api_key= "d77b4273256512b78f29f6a143fe20dc";
+const api_weather=`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${api_key}`;
+
 const data1 = {
     temperature:20,
-    weatherState:SUN,
+    weatherState:CLOUDY,
     humidity: 10,
     wind: '10 m/s',
 }
 
-
-const data2 = {
-    temperature:10,
-    weatherState:CLOUDY,
-    humidity: 40,
-    wind: '20 m/s',
-}
 class WeatherLocation extends Component{
 
     constructor(){
         super();
         this.state ={
             city: "Perú",
-            holi: data1
+            data: data1
         };
     }
+    getTemp = kelvin => {
+        return convert(kelvin).from('K').to('C').toFixed(2);
+    }
+
+    getWeatherState = weather => {
+        return CLOUDY;
+    }
+
+    getData = weather_data => {
+        const {humidity, temp} = weather_data.main;
+        const{speed} = weather_data.wind;
+        const weatherState = this.getWeatherState(this.weather);
+        const temperature = this.getTemp(temp);
+        const data = {
+            humidity,
+            temperature:temp,
+            weatherState,
+            wind:`${speed} m/s`,
+        }
+        return data;
+    };
 
     handleUpdateClick = () =>{
-        this.setState({
-            holi: data2
+        fetch(api_weather).then(data =>{
+            console.log(data);
+                return data.json();
+        }).then(weather_data =>{
+            const data= this.getData(weather_data);
+            this.setState({data});
+            console.log(weather_data);
         });
         console.log("actualizado");
     }
 
 
     render = () => {
-        const{city, holi} = this.state;
+        const{city, data} = this.state;
         return(
         <div className='weatherLocationCont'>
             <Location city ={city}/>
-            <WeatherData holi={holi} />
+            <WeatherData holi={data} />
             <button onClick={this.handleUpdateClick}>Actualizar</button>
         </div>);
 };
